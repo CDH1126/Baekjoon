974a7e30c8338034133795c4b937a8c42c3d39d0
JerryKirby <90539594+JerryKirby@users.noreply.github.com>
2024-09-24 20:07:50 +0900

    [Silver IV] Title: 제로, Time: 212 ms, Memory: 23280 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Integer> stack = new Stack<>();
+
+        int k = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < k; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                stack.pop();
+            } else{
+                stack.add(num);
+            }
+
+        }
+        int sum = 0;
+
+        for (int j : stack) {
+            sum += j;
+        }
+
+        System.out.println(sum);
+
+    }
+}
