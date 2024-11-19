87820ecc3e8e4f9dc34a5bd3f26f643be78453d4
JerryKirby <90539594+JerryKirby@users.noreply.github.com>
2024-11-19 11:13:24 +0900

    [Gold V] Title: 물병, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.java
+++ 백준/Gold/1052. 물병/물병.java
@@ -1,20 +1,44 @@
-import java.util.Scanner;
+ 
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+import java.io.*;
+import java.util.StringTokenizer;
 
-        int N = scanner.nextInt(); // 물병의 초기 개수
-        int K = scanner.nextInt(); // 목표 물병 개수
-        int addedBottles = 0;      // 추가한 물병 수
-
-        // 물병의 개수가 K 이하가 될 때까지 반복
-        while (Integer.bitCount(N) > K) {
-            N++;                 // 물병을 하나 추가
-            addedBottles++;      // 추가한 물병 수 증가
+public class Main{
+    public static void main(String[] args) throws IOException {
+        //입력값 처리하는 BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //결과값 출력하는 BufferedWriter
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int answer = 0, index = -1;
+        String bitN = Integer.toBinaryString(N);	//N ▶ 2진
+        int ones = Integer.bitCount(N);	//2진수 1의 개수
+        if(ones > K){	//1의 개수 > K 일 때
+            //K번째 '1'을 기준으로 만들어야 하는 형태의 위치 구하기
+            for (int i = 0; i < bitN.length(); i++) {
+                if (K == 0) {
+                    index = i;
+                    break;
+                }
+                if (bitN.charAt(i) == '1')
+                    K--;
+            }
+            String temp = bitN.substring(index);	//바꿔야 하는 값
+            int t = Integer.parseInt(temp, 2);	//바꿔야 하는 값의 10진수 값
+            /*
+            바꿔야 하는 값이 0이 아닐 때
+            Math.pow(2, bitN.length() - index) : 만들어야 하는 형태의 10진수 값
+            t : 바꿔야 하는 값에 10진수 값
+            Math.pow(2, bitN.length() - index) - t : 물병을 사야하는 개수
+            */
+            if (t != 0)
+                answer = (int) (Math.pow(2, bitN.length() - index) - t);
         }
-
-        System.out.println(addedBottles); // 최소 추가 물병 수 출력
-        scanner.close();
+        bw.write(answer + "");		//물병 구매 개수 BufferedWriter 저장
+        bw.flush();		//결과 출력
+        bw.close();
+        br.close();
     }
 }
