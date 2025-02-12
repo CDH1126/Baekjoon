220ebc150c5f52ffa66cd0df7c2a7091e0d17e72
JerryKirby <90539594+JerryKirby@users.noreply.github.com>
2025-02-12 16:54:45 +0900

    [Silver IV] Title: 토너먼트, Time: 104 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        int n = Integer.parseInt(input[0]);
+        int a = Integer.parseInt(input[1]);
+        int b = Integer.parseInt(input[2]);
+        int round = 0;
+
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            round++;
+        }
+
+        System.out.println(round);
+    }
+}
