bb9b392aebcce70f446a85aa634d0c0e4550b92e
JerryKirby <90539594+JerryKirby@users.noreply.github.com>
2025-03-20 13:31:00 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,26 +1,39 @@
-import java.io.*;
-import java.util.*;
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int total = 0;  //검은 영역의 넓이
-        int n = Integer.parseInt(br.readLine());  //색종이 개수
-        boolean[][] arr = new boolean[101][101];  //도화지
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            //(x,y)부터 (x+9, y+9)까지의 영역을 하나씩 체크한 후 총 넓이에 더해준다
-            for (int j = x; j < x+10; j++) {
-                for (int k = y; k < y+10; k++) {
-                    if (!arr[j][k]) {
-                        arr[j][k] = true;
-                        total++;
+
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int n = Integer.parseInt(br.readLine());
+            boolean [][] result = new boolean[100][100];
+
+            for (int i = 0; i < n; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                for (int j = a; j < a+10; j++) {
+                    for (int k = b; k < b+10; k++) {
+                        result[j][k] = true;
+                    }
+                }
+
+            }
+
+            int count = 0;
+            for (int j = 0; j < 100; j++) {
+                for (int k = 0; k < 100; k++) {
+                    if (result[j][k]) {
+                        count++;
                     }
                 }
             }
+
+            System.out.println(count);
+
         }
-        System.out.print(total);
     }
-}
