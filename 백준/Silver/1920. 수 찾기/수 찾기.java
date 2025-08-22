8d609fbc8fc7365a5a6716f05ed2dfc4dd3cf4cb
CDH1126 <90539594+CDH1126@users.noreply.github.com>
2025-08-22 14:02:49 +0900

    [Silver IV] Title: 수 찾기, Time: 540 ms, Memory: 47128 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,39 @@
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+
+    import java.util.StringTokenizer;
+    import java.util.HashSet;
+    import java.util.Set;
+
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int N = Integer.parseInt(br.readLine());
+
+            Set<Integer> set = new HashSet<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < N; i++) {
+                set.add(Integer.parseInt(st.nextToken()));
+            }
+
+            int M = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            StringBuilder sb = new StringBuilder();
+
+            for (int j = 0; j < M; j++) {
+                int numCheck = Integer.parseInt(st.nextToken());
+                
+                if (set.contains(numCheck)) {
+                    sb.append("1\n");
+                } else {
+                    sb.append("0\n");
+                }
+            }
+            System.out.println(sb);
+
+        }
+
+    }
