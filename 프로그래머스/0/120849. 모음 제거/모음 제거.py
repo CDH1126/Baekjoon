0c3001accd198bfa576b5813ab39092b1d4d5d27
CDH1126 <90539594+CDH1126@users.noreply.github.com>
2026-07-23 16:12:59 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = ''
+    
+    for i in range(len(my_string)) : 
+        if my_string[i] in "aeiou" :
+            continue
+        answer += my_string[i]
+    
+    return answer
