d9c403443b8bc2a0bcb1b2bda0422cc910406ae3
CDH1126 <90539594+CDH1126@users.noreply.github.com>
2026-07-25 10:02:31 +0900

    [level 1] Title: 소수 만들기, Time: 104.98 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,22 @@
+def solution(nums):
+    answer = 0
+    
+
+    for i in range(len(nums)) :
+        for j in range(i + 1, len(nums)) :
+            for k in range(j + 1, len(nums)) :
+                if is_prime(nums[i] + nums[j] + nums[k]) :
+                    answer += 1
+    
+
+    return answer
+
+def is_prime(n) :
+        
+        if n < 2 :
+            return False
+    
+        for i in range (2, n) :
+            if n % i == 0 :
+                return False
+        return True
