cf157408bcec47bf6560597742f535ac1994ad03
CDH1126 <90539594+CDH1126@users.noreply.github.com>
2026-08-15 00:52:03 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,31 +1,35 @@
 def solution(tickets):
-    
-    tickets.sort()
-    
-    use = [False] * len(tickets)
-    path = ["ICN"]
-    
-    def dfs(now) :
-        # 모든 티켓을 사용했을 경우 종료
-        if len(path) == len(tickets) + 1 :
+    tickets.sort()  # 알파벳 순으로 먼저 정렬
+
+    used = [False] * len(tickets)  # 각 티켓을 사용했는지 표시
+    path = ["ICN"]                 # 여행 경로
+
+    def dfs(now):
+        # 모든 티켓을 사용했다면 완성
+        if len(path) == len(tickets) + 1:
             return True
-    
-        # 모든 티켓 확인
-        for i in range(len(tickets)) :
+
+        # 모든 티켓을 하나씩 확인
+        for i in range(len(tickets)):
             start, end = tickets[i]
 
-            if start == now and not use[i] :
+            # 지금 공항에서 출발하는 티켓이고,
+            # 아직 사용하지 않은 티켓이라면
+            if start == now and not used[i]:
 
-                use[i] = True
+                # 티켓 사용
+                used[i] = True
                 path.append(end)
 
-                if dfs(end) :
+                # 다음 공항으로 이동
+                if dfs(end):
                     return True
 
-                use[i] = False
+                # 여기까지 왔는데 실패했다면 되돌아가기
+                used[i] = False
                 path.pop()
 
         return False
-    
+
     dfs("ICN")
     return path
